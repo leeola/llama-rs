44f353a350947a29feab9ca5b2e1172bbdfabf77
Lee Olayvar <leeola@fastmail.com>
2023-03-17 15:35:57 -0700

    chore: add local development nix flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url      = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            pkg-config
+            binutils
+            gcc
+            rust-analyzer
+            rust-bin.stable."1.68.0".default
+          ];
+        };
+      }
+    );
+}
